b20e617add4309d4dfde0b6be1f6e9f7143df8d7
Yennhi Dang <nhidangn@gmail.com>
2021-03-15 14:51:07 -0700

    unit testing

--- Testing.java
+++ Testing.java
@@ -1,26 +1,39 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 
 public class Testing {
+    
+        public void createTree(BinaryTree<String> tree) {
+        // Leaves:
+            BinaryTree<String> dTree = new BinaryTree<>("D");
+		    BinaryTree<String> gTree = new BinaryTree<>("G");
+		    BinaryTree<String> hTree = new BinaryTree<>("H");
+
+		// Subtrees:
+            BinaryTree<String> fTree = new BinaryTree<>("F", hTree, null);
+		    BinaryTree<String> eTree = new BinaryTree<>("E", gTree, null);
+		    BinaryTree<String> bTree = new BinaryTree<>("B", dTree, null);
+		    BinaryTree<String> cTree = new BinaryTree<>("C", eTree, fTree);
+            tree.setTree("A", bTree, cTree);
+        }
+
+              
 
     @Test
     public void testPostorderTraverse() {
-        BinaryTree<String> tree = new BinaryTree<>();
-        BinaryTree<String> dTree = new BinaryTree<>("D");
-		BinaryTree<String> eTree = new BinaryTree<>("E");
-		BinaryTree<String> gTree = new BinaryTree<>("G");
-
-		// Subtrees:
-		BinaryTree<String> fTree = new BinaryTree<>("F", null, gTree);
-		BinaryTree<String> bTree = new BinaryTree<>("B", dTree, eTree);
-		BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
-
-		tree.setTree("A", bTree, cTree);
-
-
-        @Test
-        public void testPostorderTraverse_binaryNodeMethod() {
-            
-        }
+        BinaryTree<String> aTree = new BinaryTree<>();  
+        createTree(aTree);
+        assertEquals("example output", aTree.postorderTraverse());    
     }
+
+    @Test
+    public void testPostorderTraverse_callBinaryNodeMethod() {
+        createTree(aTree);
+        assertEquals("A\nD", aTree.postorderTraverse_callBinaryNodeMethod());    
+    }
+    
     
 }
