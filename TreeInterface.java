6d3229091a4592e3cfbe90712a3eb7d11bac18a7
Yennhi Dang <nhidangn@gmail.com>
2021-03-15 19:03:23 -0700

    TreeInterface javadoc

--- TreeInterface.java
+++ TreeInterface.java
@@ -1,8 +1,24 @@
 
 public interface TreeInterface<T> {
+    
+    /** Gets the root data. */
     public T getRootData();
+
+    /** Computes the height of the whole tree.
+     * @return the height of the whole tree.
+     */
     public int getHeight();
+
+    /** Computes the number of nodes in the whole tree.
+     * @return the number of nodes in the whole tree.
+     */
     public int getNumberOfNodes();
+
+    /** Checks whether the root is empty.
+     * @return True if the root is empty, otherwise false.
+     */
     public boolean isEmpty();
+
+    /** Sets the root to null, hence the tree is empty. */
     public void clear();
 }
